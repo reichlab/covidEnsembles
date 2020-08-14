1f5bf3c3e7d74fbc9140a2fc2524890d637204c3
elray1 <evan.l.ray@gmail.com>
2020-08-14 12:29:41 -0700

    updated code for retrospective comparison of ensemble methods

--- code/application/retrospective-qra-comparison/fit_retrospective_model.R
+++ code/application/retrospective-qra-comparison/fit_retrospective_model.R
@@ -1,29 +1,33 @@
-library(quantmod)
+library(gurobi)
+library(quantgen)
 library(covidEnsembles)
 library(tidyverse)
 library(zeallot)
 library(gridExtra)
-library(zoltr)
+library(yaml)
 
-submissions_root = '~/Documents/research/epi/covid/upstream-covid19-forecast-hub/covid19-forecast-hub/data-processed/'
+submissions_root <- "~/research/epi/covid/covid19-forecast-hub/data-processed/"
 
 # List of candidate models for inclusion in ensemble
-model_dirs <- Sys.glob(paste0(submissions_root, '*'), dirmark = TRUE)
-model_dirs <- model_dirs[substr(model_dirs, nchar(model_dirs), nchar(model_dirs)) == '/']
+model_dirs <- Sys.glob(paste0(submissions_root, "*"), dirmark = TRUE)
+model_dirs <- model_dirs[
+  substr(model_dirs, nchar(model_dirs), nchar(model_dirs)) == "/"
+]
 
 model_info <- purrr::map_dfr(
   model_dirs,
   function(model_dir) {
-    metadata_path <- Sys.glob(paste0(model_dir, 'metadata*'))
+    metadata_path <- Sys.glob(paste0(model_dir, "metadata*"))
     return(as.data.frame(
-      yaml::read_yaml(metadata_path)[c('model_abbr', 'team_model_designation')],
+      yaml::read_yaml(metadata_path)[c("model_abbr", "team_model_designation")],
       stringsAsFactors = FALSE
     ))
   }
 )
 
 candidate_model_abbreviations_to_include <- model_info %>%
-  dplyr::filter(team_model_designation %in% c('primary', 'secondary', 'proposed')) %>%
+  dplyr::filter(team_model_designation %in%
+    c("primary", "secondary", "proposed")) %>%
   dplyr::pull(model_abbr)
 
 #options(warn=2, error=recover)
@@ -40,12 +44,15 @@ candidate_model_abbreviations_to_include <- model_info %>%
 #args <- c("cum_death", "2020-06-13", "FALSE", "convex", "by_location_group", "per_quantile", "4", "TRUE")
 #args <- c("cum_death", "2020-06-13", "FALSE", "convex", "by_location_group", "per_quantile", "4", "TRUE", "TRUE")
 #args <- c("inc_death", "2020-05-23", "TRUE", "positive", "mean_impute", "3_groups", "3", "FALSE", "TRUE")
+#args <- c("inc_death", "2020-07-25", "TRUE", "positive", "mean_impute", "per_quantile", "5", "TRUE", "FALSE")
+#args <- c("cum_death", "2020-05-16", "FALSE", "convex", "by_location_group", "per_quantile", "5", "FALSE", "FALSE")
+#args <- c("inc_death", "2020-07-25", "TRUE", "positive", "mean_impute", "3_groups", "3", "FALSE", "FALSE")
 
 args <- commandArgs(trailingOnly = TRUE)
 response_var <- args[1]
 forecast_week_end_date <- lubridate::ymd(args[2])
 intercept <- as.logical(args[3])
-constraint <- args[4]
+method <- args[4]
 missingness <- args[5]
 quantile_group_str <- args[6]
 window_size <- as.integer(args[7])
@@ -59,70 +66,165 @@ if(quantile_group_str == "per_model") {
 } else if(quantile_group_str == "per_quantile") {
   quantile_groups <- 1:23
 } else {
-  stop('invalid quantile_groups')
+  stop("invalid quantile_groups")
 }
 
-if(missingness == 'mean_impute') {
-  missingness <- 'impute'
-  impute_method <- 'mean'
+if(missingness == "mean_impute") {
+  missingness <- "impute"
+  impute_method <- "mean"
 } else {
   impute_method <- NULL
 }
 
-if(response_var %in% c('inc_death', 'cum_death')) {
+if(response_var %in% c("inc_death", "cum_death")) {
   required_quantiles <- c(0.01, 0.025, seq(0.05, 0.95, by = 0.05), 0.975, 0.99)
-  spatial_resolution <- c('state', 'national')
+  spatial_resolution <- c("state", "national")
   horizon <- 4L
-} else if(response_var == 'inc_case') {
+} else if(response_var == "inc_case") {
   required_quantiles <- c(0.025, 0.100, 0.250, 0.500, 0.750, 0.900, 0.975)
-  spatial_resolution <- c('county', 'state', 'national')
+  spatial_resolution <- c("county", "state", "national")
   horizon <- 4L
 }
 
-result_filename <- paste0('code/application/retrospective-qra-comparison/retrospective-fits/',
-                          response_var,
-                          '-forecast_week_', as.character(forecast_week_end_date),
-                          '-intercept_', as.character(intercept),
-                          '-constraint_', constraint,
-                          '-missingness_', missingness,
-                          '-quantile_groups_', quantile_group_str,
-                          '-window_size_', window_size,
-                          '-do_standard_checks_', do_standard_checks,
-                          '-do_baseline_check_', do_baseline_check,
-                          '.rds')
+result_filename <- paste0(
+  "code/application/retrospective-qra-comparison/retrospective-fits/",
+  response_var,
+  "-forecast_week_", as.character(forecast_week_end_date),
+  "-intercept_", as.character(intercept),
+  "-method_", method,
+  "-missingness_", missingness,
+  "-quantile_groups_", quantile_group_str,
+  "-window_size_", window_size,
+  "-do_standard_checks_", do_standard_checks,
+  "-do_baseline_check_", do_baseline_check,
+  ".rds")
 
 if(!file.exists(result_filename)) {
   do_q10_check <- do_nondecreasing_quantile_check <- do_standard_checks
-  #do_q10_check <- do_nondecreasing_quantile_check <- (response_var == 'cum_death')
-  #do_q10_check <- FALSE
-  #do_nondecreasing_quantile_check <- FALSE
-  #debug(covidEnsembles::get_imputed_ensemble_fits_and_predictions)
 
-  #c(model_eligibility, wide_model_eligibility, location_groups, component_forecasts) %<-%
-  results <-
-    build_covid_ensemble_from_local_files(
-      candidate_model_abbreviations_to_include = candidate_model_abbreviations_to_include,
-      spatial_resolution = spatial_resolution,
-      targets = paste0(1:horizon, ' wk ahead ', gsub('_', ' ', response_var)),
-      forecast_week_end_date = forecast_week_end_date,
-      timezero_window_size = 1,
-      window_size = window_size,
-      intercept = intercept,
-      constraint = constraint,
-      quantile_groups = quantile_groups,
-      missingness = missingness,
-      impute_method = impute_method,
-      backend = 'quantmod',
-      submissions_root = '~/Documents/research/epi/covid/upstream-covid19-forecast-hub/covid19-forecast-hub/data-processed/',
-      required_quantiles = required_quantiles,
-      do_q10_check = do_q10_check,
-      do_nondecreasing_quantile_check = do_nondecreasing_quantile_check,
-      do_baseline_check = do_baseline_check,
-      baseline_tol = 1.0,
-      manual_eligibility_adjust = NULL,
-      return_eligibility = TRUE,
-      return_all = TRUE
+  results <- build_covid_ensemble_from_local_files(
+    candidate_model_abbreviations_to_include =
+      candidate_model_abbreviations_to_include,
+    spatial_resolution = spatial_resolution,
+    targets = paste0(1:horizon, " wk ahead ", gsub("_", " ", response_var)),
+    forecast_week_end_date = forecast_week_end_date,
+    timezero_window_size = 1,
+    window_size = window_size,
+    intercept = intercept,
+    constraint = method,
+    quantile_groups = quantile_groups,
+    missingness = missingness,
+    impute_method = impute_method,
+    backend = "quantgen",
+    submissions_root = submissions_root,
+    required_quantiles = required_quantiles,
+    do_q10_check = do_q10_check,
+    do_nondecreasing_quantile_check = do_nondecreasing_quantile_check,
+    do_baseline_check = do_baseline_check,
+    baseline_tol = 1.0,
+    manual_eligibility_adjust = NULL,
+    return_eligibility = TRUE,
+    return_all = TRUE
+  )
+
+  # save full results including estimated weights, etc.
+  saveRDS(results, file = result_filename)
+
+  # save csv formatted forecasts
+  if (missingness == "impute") {
+    c(model_eligibility, wide_model_eligibility, location_groups,
+      weight_transfer, component_forecasts) %<-% results
+    
+    col_index <- attr(location_groups$qfm_test, "col_index")
+    models_used <- purrr::map_dfc(
+      unique(col_index$model),
+      function(model) {
+        col_ind <- min(which(col_index$model == model))
+        result <- data.frame(
+          m = !is.na(unclass(location_groups$qfm_test)[, col_ind]))
+        colnames(result) <- model
+        return(result)
+      }
+    )
+    model_counts <- apply(
+      models_used,
+      1,
+      sum
     )
 
-  saveRDS(results, file = result_filename)
+    locations_to_drop <- unique(
+      attr(location_groups$qfm_test, "row_index")$location[model_counts == 1])
+    
+    ensemble_predictions <- location_groups$qra_forecast %>%
+      dplyr::filter(!(location %in% locations_to_drop))
+  } else {
+    c(model_eligibility, wide_model_eligibility, location_groups,
+      component_forecasts) %<-% results
+    
+    model_counts <- apply(
+      location_groups %>% select_if(is.logical),
+      1,
+      sum)
+    location_groups <- location_groups[model_counts > 1, ]
+
+    if (nrow(location_groups) > 0) {
+      ensemble_predictions <- bind_rows(location_groups[['qra_forecast']])
+    }
+  }
+
+  if (nrow(ensemble_predictions) > 0) {
+    # save the results in required format
+    formatted_ensemble_predictions <- ensemble_predictions %>%
+      dplyr::transmute(
+        forecast_date = lubridate::ymd(forecast_week_end_date) + 2,
+        target = target,
+        target_end_date = calc_target_week_end_date(
+          forecast_week_end_date,
+          as.integer(substr(target, 1, 1))),
+        location = location,
+        type = 'quantile',
+        quantile = quantile,
+        value = ifelse(
+          quantile < 0.5,
+          floor(value),
+          ifelse(
+            quantile == 0.5,
+            round(value),
+            ceiling(value)
+          )
+        )
+      )
+
+    formatted_ensemble_predictions <- bind_rows(
+      formatted_ensemble_predictions,
+      formatted_ensemble_predictions %>%
+        filter(format(quantile, digits = 3, nsmall = 3) == "0.500") %>%
+        mutate(
+          type = "point",
+          quantile = NA_real_
+        )
+    )
+
+    case_str <- paste0(
+      "intercept_", as.character(intercept),
+      "-constraint_", method,
+      "-missingness_", missingness,
+      "-quantile_groups_", quantile_group_str,
+      "-window_size_", window_size,
+      "-do_standard_checks_", do_standard_checks,
+      "-do_baseline_check_", do_baseline_check)
+    
+    csv_dir <- paste0(
+      "code/application/retrospective-qra-comparison/retrospective-forecasts/",
+      case_str, "/")
+    if (!dir.exists(csv_dir)) {
+      dir.create(csv_dir)
+    }
+    csv_filename <- paste0(
+      csv_dir,
+      response_var, "-", lubridate::ymd(forecast_week_end_date) + 2, "-",
+      case_str, ".csv")
+    
+    write_csv(formatted_ensemble_predictions, csv_filename)
+  }
 }
--- code/application/retrospective-qra-comparison/run_fit_retrospective_model.R
+++ code/application/retrospective-qra-comparison/run_fit_retrospective_model.R
@@ -1,31 +1,36 @@
 library(tidyr)
 library(dplyr)
+library(readr)
 library(lubridate)
 library(doParallel)
+library(zeallot)
+library(covidEnsembles)
 
-registerDoParallel(cores = 6)
+registerDoParallel(cores = 28)
 
-output_path <- 'code/application/retrospective-qra-comparison/log/'
+output_path <- "code/application/retrospective-qra-comparison/log/"
 
 
 # basic approaches
 analysis_combinations <- tidyr::expand_grid(
-  response_var = c('cum_death', 'inc_death'),
+  response_var = c("cum_death", "inc_death"),
   forecast_week_end_date = as.character(
-    lubridate::ymd('2020-05-09') + seq(from = 0, length = 11)*7),
-  intercept = c('FALSE', 'TRUE'),
-  constraint = c('ew', 'convex', 'positive'),
-  quantile_group_str = c('per_model', '3_groups', 'per_quantile'),
-  missingness = c('mean_impute', 'by_location_group'),
+    lubridate::ymd("2020-05-09") + seq(from = 0, length = 12) * 7),
+  intercept = c("FALSE", "TRUE"),
+  constraint = c("ew", "convex", "positive"),
+  quantile_group_str = c("per_model", "3_groups", "per_quantile"),
+  missingness = c("mean_impute", "by_location_group"),
   window_size = c(0, 3:5),
-  do_standard_checks = c('FALSE', 'TRUE'),
-  do_baseline_check = c('FALSE', 'TRUE')
+  do_standard_checks = c("FALSE", "TRUE"),
+  do_baseline_check = "FALSE"
 ) %>%
   dplyr::filter(
-    (intercept == 'FALSE' & constraint == 'ew' & quantile_group_str == 'per_model' & missingness == 'by_location_group') |
-    (window_size > 0 & intercept == 'FALSE' & constraint == 'convex') |
-    (window_size > 0 & intercept == 'TRUE' & constraint == 'positive'),
-    !(do_standard_checks & do_baseline_check)
+    (intercept == "FALSE" & constraint == "ew" &
+      quantile_group_str == "per_model" & missingness == "by_location_group") |
+    (window_size > 0 & intercept == "FALSE" & constraint == "convex") |
+    (window_size > 0 & intercept == "TRUE" & constraint == "positive"),
+    !(do_standard_checks == "TRUE" & do_baseline_check == "TRUE"),
+    !(do_standard_checks == "TRUE" & response_var == "inc_death")
   )
 
 
@@ -38,9 +43,10 @@ analysis_combinations <- tidyr::expand_grid(
 #   )
 
 foreach(row_ind = seq_len(nrow(analysis_combinations))) %dopar% {
-#foreach(row_ind = seq_len(2)) %dopar% {
+# foreach(row_ind = seq_len(2)) %dopar% {
   response_var <- analysis_combinations$response_var[row_ind]
-  forecast_week_end_date <- analysis_combinations$forecast_week_end_date[row_ind]
+  forecast_week_end_date <-
+    analysis_combinations$forecast_week_end_date[row_ind]
   intercept <- analysis_combinations$intercept[row_ind]
   constraint <- analysis_combinations$constraint[row_ind]
   quantile_group_str <- analysis_combinations$quantile_group_str[row_ind]
@@ -61,42 +67,9 @@ foreach(row_ind = seq_len(nrow(analysis_combinations))) %dopar% {
     do_standard_checks, " ",
     do_baseline_check,
     "\' code/application/retrospective-qra-comparison/fit_retrospective_model.R ",
-    output_path, "output-", response_var, "-", forecast_week_end_date, '-',
-    intercept, '-', constraint, '-', missingness, '-', quantile_group_str,
-    '-', window_size, '-', do_standard_checks, '-', do_baseline_check, ".Rout")
+    output_path, "output-", response_var, "-", forecast_week_end_date, "-",
+    intercept, "-", constraint, "-", missingness, "-", quantile_group_str,
+    "-", window_size, "-", do_standard_checks, "-", do_baseline_check, ".Rout")
 
   system(run_cmd)
 }
-
-#
-# # rescaled convex approach, one weight per model
-# analysis_combinations <- tidyr::expand_grid(
-#   response_var = c('cum_death', 'inc_death'),
-#   forecast_week_end_date = as.character(
-#     lubridate::ymd("2020-04-18") + seq(from = 0, length = 8)*7),
-#   ensemble_method = 'rescaled_convex',
-#   window_size = seq(from = 1, to = 4)
-# ) %>%
-#   dplyr::filter((window_size == 0 & ensemble_method == 'ew') | window_size > 0)
-#
-#
-# foreach(row_ind = seq_len(nrow(analysis_combinations))) %dopar% {
-#   #foreach(row_ind = seq_len(2)) %dopar% {
-#   response_var <- analysis_combinations$response_var[row_ind]
-#   forecast_week_end_date <- analysis_combinations$forecast_week_end_date[row_ind]
-#   ensemble_method <- analysis_combinations$ensemble_method[row_ind]
-#   window_size <- analysis_combinations$window_size[row_ind]
-#
-#   run_cmd <- paste0(
-#     "R CMD BATCH --vanilla \'--args ",
-#     response_var, " ",
-#     forecast_week_end_date, " ",
-#     ensemble_method, " ",
-#     window_size,
-#     "\' code/application/retrospective-qra-comparison/fit_retrospective_model.R ",
-#     output_path, "output-", response_var, "-", forecast_week_end_date, '-',
-#     ensemble_method, '-', window_size, ".Rout")
-#
-#   system(run_cmd)
-# }
-#
