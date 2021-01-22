7222a247bb6f401d831daa50e091363330a9a1d4
Aaron Gerding <aaronger@users.noreply.github.com>
2021-01-22 18:11:47 -0500

    Update code/application/weekly-ensemble/build_ensembles.R
    Co-authored-by: Evan Ray <elray1@users.noreply.github.com>

--- code/application/weekly-ensemble/build_ensembles.R
+++ code/application/weekly-ensemble/build_ensembles.R
@@ -30,8 +30,8 @@ if (!file.exists(plots_root)) dir.create(plots_root, recursive = TRUE)
 # Where to save hospitalization exclusion tables
 sd_check_table_path <- 'code/application/weekly-ensemble/exclusion-outputs/tables/'
 if (!file.exists(sd_check_table_path)) dir.create(sd_check_table_path, recursive = TRUE)
-sd_check_plots_path <- 'code/application/weekly-ensemble/exclusion-outputs/plots/'
-if (!file.exists(sd_check_plots_path)) dir.create(sd_check_plots_path, recursive = TRUE)
+sd_check_plot_path <- 'code/application/weekly-ensemble/exclusion-outputs/plots/'
+if (!file.exists(sd_check_plot_path)) dir.create(sd_check_plot_path, recursive = TRUE)
 
 # List of candidate models for inclusion in ensemble
 candidate_model_abbreviations_to_include <- get_candidate_models(
